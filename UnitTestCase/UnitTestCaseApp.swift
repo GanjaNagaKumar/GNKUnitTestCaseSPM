b12d0bcef034fa75d0e310097b67a8324b438213
Kumar <ganja.nagakumar@gmail.com>
2023-07-16 23:07:54 +0530

    Initial Commit

---
+++ UnitTestCase/UnitTestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestCaseApp.swift
+//  UnitTestCase
+//
+//  Created by NagaKumar Ganja on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
